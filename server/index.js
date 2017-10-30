7ccf62f7dd8a5caa5e17df35e48369cf07bb2341
e-r-i-k-a <samuels.erika@yahoo.com>
2017-10-30 16:44:24 -0400

    started the server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 
@@ -11,4 +12,16 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use('/api', require('./apiRoutes'));
+
 app.get('*', (req, res, next) => res.sendFile(path.join(__dirname, '../public')));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(PORT, ()=> {
+  console.log('server is listening on port Andre '+ PORT)
+})
