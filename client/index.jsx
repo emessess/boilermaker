44c165032f7944515172da96074e10aa6ec9168b
Matthew Smith <mattsheridansmith@gmail.com>
2017-10-30 18:25:59 -0400

    redux complete

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,13 @@
-import react from 'react';
+import React from 'react';
 import reactDOM from 'react-dom';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
+
 
 reactDOM.render(
-  <div>
+  <Provider store={store}>
     <p>Hello World!</p>
-  </div>,
+  </Provider>,
   document.getElementById('app')
-)
+);
